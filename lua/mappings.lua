3295be2e14819413ece72bc18bb68d6137354d72
Q Xu <xuq3196@outlook.com>
2024-03-18 21:01:18 +0800

    start migration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,90 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- custom gitsigns mappings
+map("n", "<leader>gtb", function()
+    require("gitsigns").toggle_current_line_blame()
+end, { desc = "Toggle blame of each line"})
+
+-- M.gitsigns = {
+--   n = {
+--     ["<leader>gtb"] = {
+--       function()
+--         require("gitsigns").toggle_current_line_blame()
+--       end,
+--       "Toggle blame of each line",
+--     },
+--   }
+-- }
+-- -- mappings for nvim-dap
+-- M.dap = {
+--     -- key mappings work in normal mode
+--     n = {
+--       -- set breakpoints
+--       ["<leader>dbp"] = {
+--           function()
+--             require('dap').toggle_breakpoint()
+--           end,
+--           "Set breakpoints"
+--       },
+--       -- continue
+--       ["<leader>dct"] = {
+--           function()
+--             require('dap').continue()
+--           end,
+--           "Launch debug sessions/ resuming execution"
+--       },
+--       -- setp over
+--       ["<leader>dso"] = {
+--           function()
+--             require('dap').step_over()
+--           end,
+--           "Step over"
+--       },
+--       -- setp into
+--       ["<leader>dsi"] = {
+--           function()
+--             require('dap').step_into()
+--           end,
+--           "Step into"
+--       },
+--         -- evaluate expression
+--         ["<leader>dev"] = {
+--             function()
+--                 require('dapui').eval()
+--             end
+--         },
+--         -- end the debug
+--         ["<leader>ded"] = {
+--             function()
+--                 require('dap').terminate()
+--             end
+--         }
+--     },
+-- }
+--
+-- -- key mappings for neogen plugin
+-- M.neogen = {
+--     n = {
+--         ["<leader>gen"] = {
+--             function ()
+--                 require("neogen").generate()
+--             end,
+--             "Genearte annotation template"
+--         }
+--     }
+-- }
+--
+-- -- key mappings for vim-tmux-navigater
+-- M.tmux = {
+--     n = {
+--         ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+--         ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+--         ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+--         ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+--     }
+-- }
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,26 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- set a column line to indicate line changing of code
+vim.opt.colorcolumn = "80"
+
+-- set tab = 4 spaces
+-- show existing tab with 4 spaces width
+vim.opt.tabstop = 4
+-- when indenting with '>', use 4 spaces width
+vim.opt.shiftwidth = 4
+-- On pressing tab, insert 4 spaces
+vim.opt.expandtab = true
+
+-- use powershell in windows
+-- if vim.loop.os_uname().sysname == "Windows_NT" then
+--     vim.o.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+--     vim.o.shellcmdflag =
+--       "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+--     vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+--     vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+--     vim.o.shellquote = ""
+--     vim.o.shellxquote = ""
+-- end
+
