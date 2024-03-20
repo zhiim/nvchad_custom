983c9454ef352cd2421a08d6d79108d3b5a4de0e
Q Xu <xuq3196@outlook.com>
2024-03-20 15:05:54 +0800

    change to smart split

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -109,10 +109,10 @@ return {
 		-- version = "*"
 	},
 
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
+	-- {
+	-- 	"christoomey/vim-tmux-navigator",
+	-- 	lazy = false,
+	-- },
 
 	{
 		"echasnovski/mini.map",
@@ -120,6 +120,17 @@ return {
 		event = { "BufEnter" },
 		config = function()
 			require("configs.minimap")
+		end,
+	},
+
+	{
+		"mrjones2014/smart-splits.nvim",
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
 		end,
 	},
 
